160a157a9e8a157463bc27120421e0b16ccda740
SaleamlakH <saleamlak.7h@gmail.com>
2026-05-27 14:05:20 +0300

    feat: add custom error classes

---
+++ src/core/errors.ts
@@ -0,0 +1,92 @@
+export class ProjexorError extends Error {
+  code;
+
+  constructor(code: string, message: string) {
+    super(message);
+    this.code = code;
+  }
+}
+
+export class FileNotFoundError extends ProjexorError {
+  constructor(filePath: string) {
+    super('FILE_NOT_FOUND', `couldn't find a file with path ${filePath}`);
+  }
+}
+
+export class FileExistsError extends ProjexorError {
+  constructor(filePath: string) {
+    super(
+      'FILE_ALREADY_EXISTS',
+      `couldn't create ${filePath}, it is already exist`,
+    );
+  }
+}
+
+export class NodeNotFoundError extends ProjexorError {
+  constructor(nodeName: string) {
+    super(
+      'NODE_NOT_FOUND',
+      `couldn't find AST node with name ${nodeName} name `,
+    );
+  }
+}
+
+export class ParseFailedError extends ProjexorError {
+  constructor(filePath: string) {
+    super('PARSE_FAILED', `couldn't parse AST of ${filePath}`);
+  }
+}
+
+export class ParserNotFoundError extends ProjexorError {
+  constructor(parserName: string) {
+    super(
+      'PARSER_NOT_FOUND',
+      `couldn't find ${parserName} for AST parser. required to install appropriate dependency`,
+    );
+  }
+}
+
+export class UnsupportedLanguageError extends ProjexorError {
+  constructor() {
+    super('UNSUPPORTED_LANGUAGE', 'extension has no registered parser');
+  }
+}
+
+export class CommandFailedError extends ProjexorError {
+  constructor() {
+    super(
+      'COMMAND_FAILED',
+      'could not execute command, process exited with non zero code',
+    );
+  }
+}
+
+export class CommandTimeout extends ProjexorError {
+  constructor() {
+    super('COMMAND_TIMEOUT', 'command process exceed timeout');
+  }
+}
+
+export class PermissionDeniedError extends ProjexorError {
+  constructor(filePath: string) {
+    super(
+      'PERMISSION_DENIED',
+      `could not perform operation on ${filePath}, permission denied`,
+    );
+  }
+}
+
+export class InvalidOperationError extends ProjexorError {
+  constructor() {
+    super(
+      'INVALID_OPERATION',
+      'Invalid line range or malformed operation parameters',
+    );
+  }
+}
+
+export class PathOutsideRootError extends ProjexorError {
+  constructor() {
+    super('PATH_OUTSIDE_ROOT', 'Path attempts to escape the root');
+  }
+}
